3e13faab52ab04818041be6ab872b6888f9ce692
priti turkar <preetiturkrar814@gmail.com>
2026-02-19 17:14:24 +0530

    some changes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const users = require("./MOCK_DATA.json");
+const app = express();
+const PORT = 8000;
+//routes....
+
+app.get('/users', (req, res) => {
+    const html = `
+    <ul>
+    ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+});
+app.listen(PORT, ()=> console.log(`Server started at PORT: ${PORT}`));
