59ea28fe498177145a5162a91a4742bbfe2814a3
priti turkar <preetiturkrar814@gmail.com>
2026-02-19 17:27:44 +0530

    taday's last commit

--- index.js
+++ index.js
@@ -12,4 +12,15 @@ app.get('/users', (req, res) => {
     `;
     res.send(html);
 });
+
+//Rest API
+app.get("/api/users",(req, res) =>{
+    return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) =>{
+    const id =Number(req.params.id);
+    const user = users.find(user => user.id ===id);
+    return res.json(user);
+});
 app.listen(PORT, ()=> console.log(`Server started at PORT: ${PORT}`));
