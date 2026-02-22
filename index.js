8fb7e6fef8b50495c96dd22d5ece39dac9235869
priti turkar <preetiturkrar814@gmail.com>
2026-02-22 10:21:26 +0530

    add file

--- index.js
+++ index.js
@@ -14,13 +14,30 @@ app.get('/users', (req, res) => {
 });
 
 //Rest API
-app.get("/api/users",(req, res) =>{
+app.get("/api/usres",(req, res) =>{
     return res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) =>{
+app.route("/api/users/:id").get((req, res) =>{
     const id =Number(req.params.id);
     const user = users.find(user => user.id ===id);
     return res.json(user);
+})
+.patch((req, res)=>{
+    //edit user with id
+   return res.json({status: "pending"})
+})
+.delete((req, res)=>{
+    //delete user with id
+    return res.json({status: "pending"});
 });
+
+
+app.post("/api/users", (req, res) => {
+    //tood create new user 
+    return res.json({ status: "pending"});
+
+});
+
+
 app.listen(PORT, ()=> console.log(`Server started at PORT: ${PORT}`));
